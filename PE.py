5ca973d461a7ec9b764f3fb778a72bc5b02609eb
Minjun <141849879+MinjunKR1@users.noreply.github.com>
2025-02-05 19:21:24 +0900

    Update PE.py

--- PE.py
+++ PE.py
@@ -8,24 +8,32 @@ import time
 from VirusTotalAPI import scan_file as sf
 from VirusTotalAPI import results as scan_r
 
-# directory_path = r'C:\Users\jysow\Desktop\고등학교 포트폴리오\악성코드 분석기\exe'
-
-# directory_path = str(input("악성코드 파일을 분석할 격리 폴더를 생성 했습니다.\n파일을 넣으시고 enter키를 눌러서 분석을 시작해주세요.\n[Press Enter]"))
-
 def analysis(directory_path):
-    directory_path = r'{0}'.format(directory_path)
-
-    print("검역소 경로 : {0}".format(directory_path))
-
-    # 병렬 처리
     print("Executor 생성\n")
-    with ThreadPoolExecutor() as executor: # executor 생성
-        print("검역소 안에 있는 파일들을 검색하는중..\n")
-        for filename in os.listdir(directory_path): # 파일 검색
+    # DFS 깊이 우선 탐색 알고리즘으로 변경
+    with ThreadPoolExecutor() as executor:
+        print("검역소 안 파일 탐색 중..\n")
+        for filename in os.listdir(directory_path):
             file_path = os.path.join(directory_path, filename)
-            if os.path.isfile(file_path) and file_path.endswith('.exe'): # 파일 경로인지 exe 파일인지
+            if os.path.isdir(file_path):
+                analysis(file_path)
+            elif os.path.isfile(file_path) and file_path.endswith('.exe'):
                 executor.submit(analysis_file, file_path, filename)
-                print("분석 시작 : {0}".format(os.path.join(file_path, filename)))
+                print("분석 시작 : {0}".format(file_path))
+
+    # directory_path = r'{0}'.format(directory_path)
+
+    # print("검역소 경로 : {0}".format(directory_path))
+
+    # # 병렬 처리
+    # print("Executor 생성\n")
+    # with ThreadPoolExecutor() as executor: # executor 생성
+    #     print("검역소 안에 있는 파일들을 검색하는중..\n")
+    #     for filename in os.listdir(directory_path): # 파일 검색
+    #         file_path = os.path.join(directory_path, filename)
+    #         if os.path.isfile(file_path) and file_path.endswith('.exe'): # 파일 경로인지 exe 파일인지
+    #             executor.submit(analysis_file, file_path, filename)
+    #             print("분석 시작 : {0}".format(os.path.join(file_path, filename)))
 
 # 악성코드 분석
 def analysis_file(file_path, filename):
